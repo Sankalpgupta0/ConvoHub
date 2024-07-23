4332991c54d9afa0a5905081692e6f6296f840c6
Sankalpgupta0 <guptasankalp2004@gmail.com>
2024-07-24 04:16:34 +0530

    proxy settings

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy:{
-      '/api': 'https://convohub-server.onrender.com'
+      '/api': 'https://convohub-server-c6ry.onrender.com'
     }
   },
   plugins: [react()],
